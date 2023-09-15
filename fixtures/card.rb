fb5966cb721e09da8ac56377cdadc49de646c482
Y.Takano <y8otaman@gmail.com>
2023-09-15 11:55:12 +0900

    カード用関数：重複しないシャッフル機能追加

--- fixtures/card.rb
+++ fixtures/card.rb
@@ -3,3 +3,18 @@ class card
         @mycards=[]
         mycards=[0,0,0,0,0]
     end
+
+    def shuffle
+        for a in 1...5                          #トランプ五枚分シャッフル
+            x=1                                 #　フラグ　初期値:真
+            while x==1 do                       #　　フラグが真のとき、ひたすらシャッフル
+                mycards[a]=rand(1...52)         #　　１～52でシャッフル
+                for b in 1...5                  #　　判定
+                    if mycards[b]==mycards[a]   #　　　五枚それぞれ比較
+                        break                   #　　　　一つでも同じ値があればもう一度
+                    else
+                        x=0                     #　　　　判定クリア：フラグ取り下げ
+                    end
+                end
+            end
+        end

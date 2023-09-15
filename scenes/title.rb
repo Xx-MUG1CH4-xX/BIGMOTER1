5b523f67d161526ad1dcd2c205348d762681fc9a
Sakaguchi Sora <mugicha07220815s@gmail.com>
2023-09-15 16:01:48 +0900

    title

--- scenes/title.rb
+++ scenes/title.rb
@@ -3,11 +3,21 @@ class Title < Base
     super
   end
 
+ 
   def update
     super
-    Window.draw_font(100, 100, "title", Font.new(32))
+    require 'dxruby'
+    image = Image.load('C:/ruby_lecture/code/BIGMOTER1/image/a.png')  # data.pngを読み込む
+        Window.draw(0, 0, image)  # data.pngを描画する
+    Window.draw_font(300, 200, "LEVEL of G4NBLE", Font.new(50))
+
     if Input.mouse_push?(M_LBUTTON)
       @finish = true
+      require 'dxruby'
+
+      
+     
     end
+    
   end
 end

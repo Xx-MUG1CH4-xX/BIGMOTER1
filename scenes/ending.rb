4937af7f4dfab308ccfdaa0a5a71f313b9219aae
Y.Takano <y8otaman@gmail.com>
2023-09-18 14:03:21 +0900

    m9(^Д^)ﾌﾟｷﾞｬｰ

--- scenes/ending.rb
+++ scenes/ending.rb
@@ -3,7 +3,7 @@ class Ending<Base
     def initialize
       super
       @timer_aori=0
-      @text_aori="ww☆"
+      @text_aori="wwww☆"
       @text_gameover="Game Over..."
       @cnt_aori=0
     end
@@ -11,24 +11,26 @@ class Ending<Base
     def update
       super
       @timer_aori+=1
-      #image = Image.load('image/a.png')  # data.pngを読み込む
-      #Window.draw(0, 0, image)  # data.pngを描画する
       
-      if @timer_aori>=120 || @timer_aori<180
+      if @timer_aori>=120 && @timer_aori<180
         Window.draw_font(700, 705, "Let's go to hell !", Font.new(15))
-      end
-      if @timer_aori>=60 || @timer_aori<180
-        Window.draw_font(700, 720, "Acom,Promiss,Shinsei-Bank > Real world", Font.new(15))
         @text_gameover="m9(^Д^)プギャー"
+      end
+      if @timer_aori>=60 && @timer_aori<180
+        Window.draw_font(700, 720, "Acom,Promiss,Shinsei-Bank > Real world", Font.new(15))
+        
       end
       if @timer_aori>=180
         Window.draw_font(0,0, @text_aori, Font.new(15))
-        @text_aori="ww"+@text_aori
-        if (@timer_aori-180+2)%48==0
+        @text_aori="wwww"+@text_aori
+        Window.draw_font(450-(@timer_aori-180)/8, 750/2-(@timer_aori-180)/16, @text_gameover, Font.new(15+(@timer_aori-180)/16))
+        if (@timer_aori-180+2)%24==0
             @text_aori="☆\n☆"+@text_aori
             @cnt_aori+=1
+            
+            
         end
-        if @cnt_aori>=40
+        if @cnt_aori>=48
             Window.close
         end
       end
@@ -37,8 +39,10 @@ class Ending<Base
         @finish = true
       end
       
-      Window.draw_font(450, 750/2, @text_gameover, Font.new(15))
-      Window.draw_font(820, 735, "Continue > Click", Font.new(15))
+      if @timer_aori<=180
+        Window.draw_font(450, 750/2, @text_gameover, Font.new(15))
+        Window.draw_font(820, 735, "Continue > Click", Font.new(15))
+      end
     end
 
     def next_scene
